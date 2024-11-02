cb6030c782fc50bc21af04dc93c0a85872c0180f
tusharpandey151 <tusharpandey151@gmail.com>
2024-11-02 15:07:14 +0000

    functions-partial-2

--- functions.py
+++ functions.py
@@ -16,3 +16,19 @@ print("Default last name " + get_greet(first_name="Tushar"))#keyword argument be
 
 print(greet("Tushar" , "Pandey"))# here we get none -- as this function does not return anything.. all functions return null by default unless we return something explicitly
 
+def multiply(*numbers):# Takes multiple entries - these entries are passed as tuple
+    i = 1
+    for number in numbers:
+        i*=number
+    return i
+
+print(multiply(3,5,6,7,8))
+
+
+def save_user(**userData): # using double * again takes multiple entries but with keyword argument making the input type a dictionary
+    print("ID: ", userData["id"])
+    print("Name: ", userData['name'])
+    print("Age : ", userData["age"])
+
+
+save_user(id=345, name="Tushar Pandey", age = 31)
